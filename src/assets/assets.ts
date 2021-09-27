057dae05a28f7ac0c2673783e417248bd3f01215
kharoh <Gaulu03@gmail.com>
2021-09-27 14:50:43 +0200

    began to style the first screen

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,3 +1,3 @@
 export default {
-
+  photoDeClasse: require('./images/class.jpg').default
 } as const
--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -7,7 +7,32 @@ const HomePage = (props: HomePageProps) => {
 
   return (
     <div id="HomePage">
-      {/* voir maquette */}
+      <header>
+        <img src={assets.photoDeClasse} alt="La photo de classe." />
+      </header>
+      <div className="title">
+        <h1>Sigma - MPSI 1</h1>
+        <p className="subtitle">depuis 1854</p>
+      </div>
+      <nav>
+      </nav>
+      <section className="presentation">
+        <article className="ginette">
+          <div className="left-part"></div>
+          <div className="right-part"></div>
+        </article>
+        <article className="sigma">
+          <div className="left-part"></div>
+          <div className="right-part"></div>
+        </article>
+        <article className="traditions">
+          <div className="left-part"></div>
+          <div className="right-part"></div>
+        </article>
+      </section>
+      <section className="students"></section>
+      <section className="matters"></section>
+      <footer></footer>
     </div>
   )
 }
